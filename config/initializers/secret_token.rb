c1d316ab2b11d27ac1d07f8a2d920b46135fced2
qxm <qxm@ninetree.(none)>
2013-03-03 20:34:07 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '7d549a66edd19012ee55aa1386126bd3d0712aacd51826de9166eed5955417c3197611848069fdadbca643ab07a5b0d7133b428443069ce80a49c66e16636f4d'
